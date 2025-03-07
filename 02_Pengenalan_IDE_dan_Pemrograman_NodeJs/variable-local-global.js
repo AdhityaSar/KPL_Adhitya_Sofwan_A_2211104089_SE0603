f90005a17faeb8cf8044f4c738795c83a5eff82c
AdhityaSar <adhitya.sar@gmail.com>
2025-03-07 08:36:18 +0700

    First Commit

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variable-local-global.js
@@ -0,0 +1,12 @@
+let globalVarible = "Kucing"
+
+function getMyIdentity() {
+    let localVariable = 2211104089
+
+    console.log(`Nama saya ${globalVarible} nim ${localVariable}`)
+}
+
+console.log(globalVarible)
+// console.log(localVariable)
+
+getMyIdentity()
---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variables.js
@@ -0,0 +1,15 @@
+var name = "Kucing"
+const nim = 2211104089
+
+console.log(name)
+
+console.log("nama saya adalah " + name + " dan nim saya adalah " + nim)
+console.log(`Nama saya adalah ${name} dan nim saya adalah ${nim}`)
+
+console.log(`Nama saya adalah ${name}
+    nim saya adalah ${nim}`)
+
+
+var name = "ayam"
+
+let fullName = "Muhammad Rizqi"
